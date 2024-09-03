f640cc9c6039a6227a066a932290747f5042b1ea
Denis Poznyakov <flint037@gmail.com>
2024-09-03 16:07:47 +0300

    commit

---
+++ advertisements/views.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.models import Advertisement
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated(), ]
+        # if self.request.method == 'PATCH':
+        #     return [IsOwnerOrReadOnly(), ]
+        return []
+
